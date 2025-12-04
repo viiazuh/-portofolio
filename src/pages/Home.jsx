967da68f74d1bc5d7300d265e36d8894812cbf13
Azuzu <vioazuhri22@gmail.com>
2025-12-04 22:39:29 +0700

    Update portfolio: Add freelance project & new skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
                     <Link to="/projects" className="group bg-slate-900 text-white px-8 py-4 rounded-full font-bold shadow-xl shadow-slate-900/30 hover:bg-slate-800 transition-all hover:-translate-y-1 flex items-center gap-2">
                         VIEW PROJECTS <ArrowRight size={20} />
                     </Link>
-                    <a href="public/VioAzuhri.pdf" download="CV-VioAzuhri.pdf" className="group bg-white border-2 border-slate-200 px-8 py-4 rounded-full font-bold hover:border-slate-900 transition-colors flex items-center gap-2">
+                    <a href="VioAzuhri.pdf" download="CV-VioAzuhri.pdf" className="group bg-white border-2 border-slate-200 px-8 py-4 rounded-full font-bold hover:border-slate-900 transition-colors flex items-center gap-2">
                         Download CV <Download size={20} className="group-hover:translate-y-1 transition-transform"/>
                     </a>
                 </div>
